e9cd955b7eda3bc4670ff061f8fd3c4fe36d3797
alexanderaliferis <alexander.daliferis@gmail.com>
2019-12-05 23:33:48 +0100

    Initial commit

---
+++ Google trend.py
@@ -0,0 +1,28 @@
+from DD import *
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+import datetime
+
+google_username = ""
+google_password = ""
+path = ""
+
+# Login to Google. Only need to run this once, the rest of requests will use the same session.
+pytrend = TrendReq(google_username, google_password)
+
+
+#sc = StandardScaler()
+sc = MinMaxScaler()
+
+gtrends = get_daily_data('S&P500',2013,1,2013,8)
+gtrends = gtrends.drop(['isPartial'], axis='columns')
+df_scaled = gtrends.copy(deep=True)
+#df_scaled['S&P500'] = sc.fit_transform(df_scaled['S&P500'].values.reshape(-1,1))
+print("hi")
+df_scaled.to_csv("S&P500_trend.csv", index=False)
+
+
+
+
+
+
---
+++ functions.py
@@ -0,0 +1,55 @@
+from pandas import read_csv
+from pandas import datetime
+from matplotlib import pyplot
+from sklearn.preprocessing import StandardScaler
+import tensorflow as tf
+tf.reset_default_graph()
+lstm_graph = tf.Graph()
+with lstm_graph.as_default():
+
+
+"""
+def ml_scaler():
+
+
+
+
+
+def new_timewindow():
+
+"""
+
+
+
+#def index_windows():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print('Running stock predictor test')
